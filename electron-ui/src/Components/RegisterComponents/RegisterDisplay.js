4f5d3c38e14612051c0e9815e457623ada057c00
Dylan Morris <drm54@students.uwf.edu>
2021-10-01 16:34:58 -0500

    Added base registration page and routes

---
+++ electron-ui/src/Components/RegisterComponents/RegisterDisplay.js
@@ -0,0 +1,45 @@
+import { Yes, RegisterDisplayContainer } from "../../Styles/RegisterStyles";
+
+function RegisterDisplay () {
+  return (
+    <Yes>
+      <RegisterDisplayContainer>
+        <div class="register">
+          <h1>Welcome to Anonym-us</h1>
+          <h2>Register Below</h2>
+          <form method="post">
+            <input
+                type="text"
+                name="fname"
+                placeholder="First Name"
+                required="required"
+            />
+            <input
+                type="text"
+                name="lname"
+                placeholder="Last Name"
+                required="required"
+            />
+            <input
+              type="text"
+              name="u"
+              placeholder="Username"
+              required="required"
+            />
+            <input
+              type="password"
+              name="p"
+              placeholder="Password"
+              required="required"
+            />
+            <button type="submit" class="btn btn-dark btn-block btn-large ">
+              Complete Registration
+            </button>
+          </form>
+        </div>
+      </RegisterDisplayContainer>
+    </Yes>
+  );
+}
+
+export default RegisterDisplay;
---
+++ electron-ui/src/Pages/RegisterPage.js
@@ -0,0 +1,9 @@
+import RegisterDisplay from "../Components/RegisterComponents/RegisterDisplay";
+
+function Register(){
+    return(
+        <RegisterDisplay/>
+    )
+}
+
+export default Register;
