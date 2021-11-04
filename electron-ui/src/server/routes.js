ffae825d4160d11a7bc100f337cde9650d8552e1
Trent Endresen <trent.endresen@IC-C02FV3EPMD6P.local>
2021-11-04 17:25:19 -0500

    Got backend api calls working to allow for logging in

---
+++ electron-ui/src/server/routes.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const router = express.Router();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const db = require("../database/db.js");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/getall", (req, res) => {
+  db.getAllUsers().then(function (results) {
+    res.send(results);
+    return results;
+  })
+    .catch(function (err) {
+      res.send(err);
+    })
+});
+
+app.post("/login", (req, res) => {
+  db.validateUser(req.body.un, req.body.pw).then(function (results) {
+    res.send(results);
+    return results;
+  })
+    .catch(function (err) {
+      res.send(err);
+    })
+})
+
+app.get("/express", (req, res) => {
+  res.send({ express: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+}); 
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
