21daaef12449aa49c1374f497892d8840d782b42
Dylan Morris <drm54@students.uwf.edu>
2021-11-09 21:20:29 -0600

    Added landing page navbar

--- electron-ui/src/Components/LandingComponents/LandingDisplay.js
+++ electron-ui/src/Components/LandingComponents/LandingDisplay.js
@@ -4,6 +4,15 @@ function LandingDisplay() {
   return (
     <Yes>
       <LandingDisplayContainer>
+        <ul>
+          <div class="topnav">
+            <li><a class="active" href="#home">Home</a></li>
+            <li><a href="#accounts">Anonym-Us Accounts</a></li>
+            <li><a href="#profile">Settings</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li><a class="right" href="#logout">Logout</a></li>
+          </div>
+        </ul>
         <div class="login">
           <h1>Welcome to the landing page</h1>
         </div>
